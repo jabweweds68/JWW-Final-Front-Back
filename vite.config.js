5a081f9a5fb0784fbcecdf99cb2613a42a4ee399
Meer Mohsin <meermohsin877@gmail.com>
2025-10-30 23:05:06 +0500

    push

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
 
-import { defineConfig } from 'vite';
-export default defineConfig({
-  root: 'src', // 
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: 'src/index.html',
-        About: 'src/About.html',
-        contact: 'src/contact.html',
-        products: 'src/product-page.html',
-        checkout: 'src/checkout.html',
-        thankyou:'src/thankyou.html',
-        dashboard:'src/Dashboard.html',
-        login:'src/Login.html',
-        orders:'src/Orders.html',
-        countdown: 'src/count-down.html'
-      }
-    }
-  },
-});
+// import { defineConfig } from 'vite';
+// export default defineConfig({
+//   root: 'src', // 
+//   build: {
+//     outDir: '../dist',
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       input: {
+//         index: 'src/index.html',
+//         About: 'src/About.html',
+//         contact: 'src/contact.html',
+//         products: 'src/product-page.html',
+//         checkout: 'src/checkout.html',
+//         thankyou:'src/thankyou.html',
+//         dashboard:'src/Dashboard.html',
+//         login:'src/Login.html',
+//         orders:'src/Orders.html',
+//         countdown: 'src/count-down.html'
+//       }
+//     }
+//   },
+// });
